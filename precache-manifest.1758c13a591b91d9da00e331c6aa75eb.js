1bab64d69922df30e46a62acd1b16c7e8afec7e8
Drew11 <drew.naumenko@gmail.com>
2019-06-14 13:44:25 +0300

    Updates

---
+++ precache-manifest.1758c13a591b91d9da00e331c6aa75eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4afbdfeefbe2db70b58c0c73d810faa",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "5bd789800a45d4add394",
+    "url": "/todo-app/static/css/main.e4b6b0f2.chunk.css"
+  },
+  {
+    "revision": "609c56dfd95f9cf6673d",
+    "url": "/todo-app/static/js/2.c1ec755e.chunk.js"
+  },
+  {
+    "revision": "5bd789800a45d4add394",
+    "url": "/todo-app/static/js/main.0840aadd.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "6ef2b84790dbffc19d0e8bbfafbc861e",
+    "url": "/todo-app/static/media/checked.6ef2b847.svg"
+  },
+  {
+    "revision": "40e0f797929ce7b185ed654ae67b7f13",
+    "url": "/todo-app/static/media/unchecked.40e0f797.svg"
+  }
+]);
